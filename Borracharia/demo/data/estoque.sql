3bb15c4eebdae48304a0e7e9b27bdb6744207321
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2026-04-09 13:20:50 -0300

    Criando o BD e atualizando o relacionamento das tabelas  no README

---
+++ Borracharia/demo/data/estoque.sql
@@ -0,0 +1,82 @@
+drop database estoque;
+create database estoque;
+use estoque;
+
+create table usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    perfil VARCHAR(50) NOT NULL
+);
+
+create table fornecedor (
+ id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20)
+);
+
+create table categoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+create table produto (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    valor_custo DECIMAL(10,2),
+    valor_venda DECIMAL(10,2) NOT NULL,
+    quantidade_estoque INT NOT NULL DEFAULT 0,
+
+    categoria_id INT,
+    fornecedor_id INT,
+
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id)
+);
+
+create table entrada_produto (id INT AUTO_INCREMENT PRIMARY KEY,
+    produto_id INT NOT NULL,
+    fornecedor_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    data DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (produto_id) REFERENCES produto(id),
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+	
+create table venda (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    data DATETIME DEFAULT CURRENT_TIMESTAMP,
+    forma_pagamento VARCHAR(50),
+
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+create table venda_produto(  
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    venda_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    valor_unitario DECIMAL(10,2) NOT NULL,
+    subtotal DECIMAL(10,2) NOT NULL,
+
+    FOREIGN KEY (venda_id) REFERENCES venda(id),
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
+);
+
+create table movimentacao_estoque(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    produto_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    tipo VARCHAR(20) NOT NULL,      /* ENTRADA / SAIDA*/
+    origem VARCHAR(20) NOT NULL,    /* COMPRA / VENDA*/
+    quantidade INT NOT NULL,
+    data DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (produto_id) REFERENCES produto(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SistemaEstoqueBorracharia
 Um sistema para gerenciar um estoque de uma borracharia e logo depois fazer uma integração completa com o cliente, enviando notificações e recomendações
+
+Para os Relacionamentos no Banco de Dados, utilizei o seguinte modelo mental:
+
+Fornecedor 1 ─── N Produto
+Categoria 1 ─── N Produto
+
+Produto 1 ─── N EntradaProduto
+Produto 1 ─── N VendaProduto
+
+Venda 1 ─── N VendaProduto
+
+Usuário 1 ─── N Venda
+Usuário 1 ─── N Entrada
+Usuário 1 ─── N Movimentação
